1b60ecf6fff0cf2987c032a29b30c7b14b94104f
LucasHaug <lucas.haug@hotmail.com>
2019-04-27 11:16:56 -0300

    Change tests

--- generate_tests.fish
+++ generate_tests.fish
@@ -1,5 +1,5 @@
 for i in (seq 100)
-    set -l entrada (python tudo.py)
+    set -l entrada (python generate_tests.py)
     set -l saida (echo "$entrada" | ./ep1 | grep -oE "[[:digit:]]+")
 
     echo teste $i \""$entrada"\" \""$saida"\"
